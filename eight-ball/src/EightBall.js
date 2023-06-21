dcad7876537bae3f3579587e0a8e40e95ec82a00
Cindy Lin <linc98689@gmail.com>
2023-06-21 11:36:31 -0400

    completed eight-ball

---
+++ eight-ball/src/EightBall.js
@@ -0,0 +1,44 @@
+import React, {useState} from 'react';
+import './EightBall.css';
+
+const data = [
+    { msg: "It is certain.", color: "green" },
+    { msg: "It is decidedly so.", color: "green" },
+    { msg: "Without a doubt.", color: "green" },
+    { msg: "Yes - definitely.", color: "green" },
+    { msg: "You may rely on it.", color: "green" },
+    { msg: "As I see it, yes.", color: "green" },
+    { msg: "Most likely.", color: "green" },
+    { msg: "Outlook good.", color: "green" },
+    { msg: "Yes.", color: "green" },
+    { msg: "Signs point to yes.", color: "goldenrod" },
+    { msg: "Reply hazy, try again.", color: "goldenrod" },
+    { msg: "Ask again later.", color: "goldenrod" },
+    { msg: "Better not tell you now.", color: "goldenrod" },
+    { msg: "Cannot predict now.", color: "goldenrod" },
+    { msg: "Concentrate and ask again.", color: "goldenrod" },
+    { msg: "Don't count on it.", color: "red" },
+    { msg: "My reply is no.", color: "red" },
+    { msg: "My sources say no.", color: "red" },
+    { msg: "Outlook not so good.", color: "red" },
+    { msg: "Very doubtful.", color: "red" },
+  ];
+
+const initElement = {msg:"Think of a Question", color:"black"};
+const EightBall = ()=>{
+    const [element, setElement] = useState(initElement); 
+    return (
+        <div  className="EightBall" 
+            onClick={()=>setElement(randomChoice(data))} 
+            style={{backgroundColor:element.color}}> 
+            <p className="EightBall-msg">{element.msg}</p>
+        </div>
+    )
+}
+
+const randomChoice = (arr)=>{
+    let idx = Math.floor(Math.random() * arr.length);
+    return arr[idx];
+};
+
+export default EightBall;
